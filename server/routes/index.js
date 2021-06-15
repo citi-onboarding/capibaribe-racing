5c40e2ec89e597cc13b37bd44bc00aa9d3322450
Aslay Clevisson <aslayclei@gmail.com>
2021-06-15 20:09:52 -0300

    Hotfix(feature/about-the-car): poster removed

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,7 @@ const path = require('path');
 const keystone = require('keystone');
 const cors = require('cors');
 
-const postController = require('../controllers/postController.js');
 const aboutFormulaSaeController = require('../controllers/aboutFomulaSaeController.js');
-
-
-//const Post = keystone.list('Posts');
 
 module.exports = (app) => {
   app.use(cors());
@@ -14,8 +10,6 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
-
-  app.get('/api/posts', postController.getPost);
 
   app.get('/api/about-formula-sae', aboutFormulaSaeController.getAboutFormulaSae);
 
