d59ec2b8850d53e663ed8a8b5fc451b76abf5e42
Aslay Clevisson <aslayclei@gmail.com>
2021-06-15 20:27:32 -0300

    Hotfix(feature/backers): poster removed

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,7 @@ const path = require('path');
 const keystone = require('keystone');
 const cors = require('cors');
 
-const postController = require('../controllers/postController.js');
-
 const backersController = require('../controllers/backersController.js');
-
-
-
-
-//const Post = keystone.list('Posts');
 
 module.exports = (app) => {
   app.use(cors());
@@ -17,8 +10,6 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
-
-  app.get('/api/posts', postController.getPost);
 
   app.get('/api/backers', backersController.getBackers);
 
