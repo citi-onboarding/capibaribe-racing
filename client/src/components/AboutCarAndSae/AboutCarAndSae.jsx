8be423463dc892486ed8ac8ea1646c1342b2ad4a
Aslay Clevisson <aslayclei@gmail.com>
2021-06-22 10:40:28 -0300

    Feature(feature/about-the-car-section-client): added banner backgorund

--- client/src/components/AboutCarAndSae/AboutCarAndSae.jsx
+++ client/src/components/AboutCarAndSae/AboutCarAndSae.jsx
@@ -12,13 +12,13 @@ function AboutInfo() {
   const loadAboutCar = async () => {
     const res = await apiAxios.get("about-car");
     setAboutCar(res.data);
-    /*console.log(res);*/
+    console.log(res.data);
   };
 
   const loadAboutFormulaSae = async () => {
     const res = await apiAxios.get("about-formula-sae");
     setAboutFormulaSae(res.data);
-    console.log(res);
+    console.log(res.data[0].image);
   };
 
   useEffect(() => {
@@ -27,45 +27,46 @@ function AboutInfo() {
   }, []);
 
   return (
-    <section className="container" id={styles.aboutInfo}>
-        <header id={styles.navBar}>
-            <div className={styles.navContent} id={styles.headerContainer}>
-                <nav>
-                    <ul className={styles.NavbarAbout}>
-                        <li>
-                            <a href='#' onClick={() => setMenu(true)} className="fontTitleSection">
-                                <h3>NOSSO CARRO</h3>
-                            </a>
-                        </li>
-                        <li>
-                            <a href='#' onClick={() => setMenu(false)} className="fontTitleSection">
-                                <h3>FÓRMULA SAE</h3>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+    <section className={styles.BackgroundSection} style={{ backgroundImage: `url(${menu ? (aboutcar.map((d) => d.image.secure_url)) : (formulasae.map((d) => d.image.secure_url)) })` }}>
+      <div className="container" id={styles.aboutInfo}>
+          <div className={styles.navContent}>
+              <nav className={styles.NavbarAbout}>
+                  <ul className={styles.NavbarList}>
+                      <li>
+                          <a href='#' onClick={() => setMenu(true)} className="fontTitleSection">
+                              <h3>NOSSO CARRO</h3>
+                          </a>
+                      </li>
+                      <li>
+                          <a href='#' onClick={() => setMenu(false)} className="fontTitleSection">
+                              <h3>FÓRMULA SAE</h3>
+                          </a>
+                      </li>
+                  </ul>
+              </nav>
+          </div>
 
-
-        {menu ?
-        (<div className={styles.gridSectionAboutInfoContent}>
-            <div className={styles.articleAboutInfo}>
-                <h2 className="fontTitleSection" id={styles.underline}>NOSSO CARRO</h2>
-                <p className="fontDescription">
-                  {aboutcar.map((d) => d.description)}
-                </p>
-            </div>
-        </div>)
-        :
-        (<div className={styles.gridSectionAboutInfoContent}>
-            <div className={styles.articleAboutInfo}>
-                  <h2 className="fontTitleSection" id={styles.underline}>Fórmula SAE</h2>
+          {menu ?
+          (<div className={styles.gridSectionAboutInfoContent}>
+              <div/>
+              <div className={styles.articleAboutInfo}>
+                  <h2 className="fontTitleSection" id={styles.underline}>NOSSO CARRO</h2>
                   <p className="fontDescription">
-                    {formulasae.map((d) => d.description)}
+                    {aboutcar.map((d) => d.description)}
                   </p>
               </div>
+          </div>)
+          :
+          (<div className={styles.gridSectionAboutInfoContent}>
+              <div/>
+              <div className={styles.articleAboutInfo}>
+                    <h2 className="fontTitleSection" id={styles.underline}>Fórmula SAE</h2>
+                    <p className="fontDescription">
+                      {formulasae.map((d) => d.description)}
+                    </p>
+                </div>
           </div>)};
+      </div>
     </section>
     );
 }
