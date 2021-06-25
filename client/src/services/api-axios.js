fd2b52f6653f303e6634ae34e1c3c3648e58194d
Aslay Clevisson <aslayclei@gmail.com>
2021-06-25 02:23:29 -0300

    Feature(deploy): base url

--- client/src/services/api-axios.js
+++ client/src/services/api-axios.js
@@ -5,7 +5,7 @@ const prod = process.env.NODE_ENV === production;
 let apiAxios;
 
 if (prod){
-  apiAxios = axios.create({baseURL: "http://url.com/api/"});
+  apiAxios = axios.create({baseURL: "https://capibariberacing.herokuapp.com/api/"});
 } else {
   apiAxios = axios.create({baseURL: "http://localhost:3001/api/"});
 }
