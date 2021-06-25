809a5fcbdfd0445ce0bd0e2a4247185035fbb6e1
Aslay Clevisson <aslayclei@gmail.com>
2021-06-25 00:17:28 -0300

    Feature(main): added package lock and production enviroment

--- client/src/services/api-axios.js
+++ client/src/services/api-axios.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
-const apiAxios = axios.create({
-  baseURL: "http://localhost:3001/api/"
-});
+const prod = process.env.NODE_ENV === production;
+
+let apiAxios;
+
+if (prod){
+  apiAxios = axios.create({baseURL: "http://url.com/api/"});
+} else {
+  apiAxios = axios.create({baseURL: "http://localhost:3001/api/"});
+}
 
 export default apiAxios;
