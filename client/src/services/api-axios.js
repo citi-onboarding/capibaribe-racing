3d8a3ba186afe51540800fd121f75b273e8f224e
Aslay Clevisson <aslayclei@gmail.com>
2021-06-25 02:59:03 -0300

    hotfix(deploy): modified routes/server

--- client/src/services/api-axios.js
+++ client/src/services/api-axios.js
@@ -5,9 +5,9 @@ const prod = process.env.NODE_ENV === production;
 let apiAxios;
 
 if (prod){
-  apiAxios = axios.create({baseURL: "https://capibariberacing.herokuapp.com/api/"});
+  apiAxios = axios.create({baseURL: "https://capibariberacing.herokuapp.com"});
 } else {
-  apiAxios = axios.create({baseURL: "http://localhost:3001/api/"});
+  apiAxios = axios.create({baseURL: "http://localhost:3001"});
 }
 
 export default apiAxios;
