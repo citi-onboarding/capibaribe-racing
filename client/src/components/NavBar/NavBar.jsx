1131cc30341651764404ef553fe9170972f89090
Aslay Clevisson <aslayclei@gmail.com>
2021-06-25 06:14:06 -0300

    Feature(navBar-Client): added new elements and effects minor tweaks

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,51 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect  } from "react";
 
 import logoCapibaNavbar from "../../assets/logo-navbar.svg";
 import MenuHamb from "../../assets/sair.svg";
 import Sair from "../../assets/menu-hamburguer.svg";
-
+import apiAxios from '../../services/api-axios.js';
 
 import styles from "./navBar.module.css";
 
 function NavBar() {
   const [menu, setMenu] = useState(true);
-  const [clickMenu, setClickMenu] = useState(false);
+  const [conteudos, setConteudos] = useState([]);
+
+  const loadConteudos = async () => {
+    const res = await apiAxios.get("aboutUs");
+    setConteudos(res.data);
+    console.log(res.data);
+  };
+
+  useEffect(() => {
+    loadConteudos();
+  }, []);
 
   return (
-    <header className="fontNavbar" id={styles.Navbar}>
-        <nav className={`container ${styles.Menu}`}>
-          <div className={styles.LogoNav}>
-            <img src={logoCapibaNavbar} alt="Logo Capiba"/>
-          </div>
+    <div>
+      <input type="checkbox" className={styles.checkMode} id="check"/>
+      <header className="fontNavbar" id={styles.Navbar}>
+          <nav className={`container ${styles.Menu}`}>
+            <div className={styles.LogoNav}>
+              <img src={logoCapibaNavbar} alt="Logo Capiba"/>
+            </div>
 
-          <ul className={styles.NavBarList}>
-            <li><a href="#banner">Sobre nós</a></li>
-            <li><a href="#infoCarAndSae">Nosso Carro</a></li>
-            <li><a href="#infoCarAndSae">Fórmula SAE</a></li>
-            <li><a href="#Sponsors">Patrocinadores</a></li>
-            <li><a href="#contactUs">Contato</a></li>
-          </ul>
+            <ul className={styles.NavBarList}>
+              <li><a href="#banner">Sobre nós</a></li>
+              <li><a href="#infoCarAndSae">Nosso Carro</a></li>
+              <li><a href="#infoCarAndSae">Fórmula SAE</a></li>
+              <li><a href="#Sponsors">Patrocinadores</a></li>
+              <li><a href="#contactUs">Contato</a></li>
+            </ul>
 
-          <input type="checkbox" className={styles.checkMode} id="check"/>
-          <div className={styles.Bars} onClick={() => setMenu(!menu)}>
-            {menu ?
-            (<label htmlFor={styles.check}><img src={Sair} alt="Sair" /></label>)
-            :
-            (<label htmlFor={styles.check}><img src={MenuHamb} alt="Hamburguer Menu" /></label>)}
-          </div>
-        </nav>
-        {menu ? null :
-        (<nav className={styles.MenuResponsive}>
+            <div className={styles.Bars} onClick={() => setMenu(!menu)}>
+              {menu ?
+              (<label htmlFor="check"><img src={Sair} alt="Sair" /></label>)
+              :
+              (<label htmlFor="check"><img src={MenuHamb} alt="Hamburguer Menu" /></label>)}
+            </div>
+          </nav>
+          {menu ? null :
+          (<nav className={styles.MenuResponsive}>
 
-          <ul className={styles.MenuResponsiveList}>
-            <li><a onClick={() => setMenu(!menu)} href="#banner">Sobre nós</a></li>
-            <li><a onClick={() => setMenu(!menu)} href="#infoCarAndSae">Nosso Carro</a></li>
-            <li><a onClick={() => setMenu(!menu)} href="#infoCarAndSae">Fórmula SAE</a></li>
-            <li><a onClick={() => setMenu(!menu)} href="#">Patrocinadores</a></li>
-            <li><a onClick={() => setMenu(!menu)} href="#">Contato</a></li>
-          </ul>
-        </nav>)}
-    </header>
+            <ul className={styles.MenuResponsiveList}>
+              <li><a onClick={() => setMenu(!menu)} href="#banner">Sobre nós</a></li>
+              <li><a onClick={() => setMenu(!menu)} href="#infoCarAndSae">Nosso Carro</a></li>
+              <li><a onClick={() => setMenu(!menu)} href="#infoCarAndSae">Fórmula SAE</a></li>
+              <li><a onClick={() => setMenu(!menu)} href="#">Patrocinadores</a></li>
+              <li><a onClick={() => setMenu(!menu)} href="#">Contato</a></li>
+            </ul>
+
+            <div className={styles.MoreContent}>
+              <a href={conteudos.map((d) => d.content)} target="_blank">NOSSOS CONTEÚDOS</a>
+            </div>
+          </nav>)}
+      </header>
+    </div>
   );
 }
 
